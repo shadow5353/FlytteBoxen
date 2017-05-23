2c40d9e932b967a642d5f89297c9d9896f3dfe94
Jacob Hinze <kontakt@jacobhinze.dk>
2017-05-23 17:44:25 +0200

    Added MainFrame

--- src/TestClass.java
+++ src/TestClass.java
@@ -1,11 +1,27 @@
+import GUI.MainFrame;
+
 /**
  * Created by Jacob on 23-05-2017.
  */
 public class TestClass {
     public static void main(String[] args) {
-        String userName = System.getProperty("user.name");
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(MainFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(MainFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(MainFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(MainFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
 
-        System.out.println(userName);
-
+        new MainFrame().setVisible(true);
     }
 }
