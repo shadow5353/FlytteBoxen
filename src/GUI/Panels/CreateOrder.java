1a389748966178bf184f09e92ce3a40591f6d60b
Jacob <jacob.hinze@hotmail.com>
2017-06-06 18:26:26 +0200

    Finished work in CreateOrder

--- src/GUI/Panels/CreateOrder.java
+++ src/GUI/Panels/CreateOrder.java
@@ -1,31 +1,59 @@
 package GUI.Panels;
 
+import GUI.Frames.AvailableBoxes;
+import Tech.Messages;
 import org.jdesktop.swingx.JXDatePicker;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Date;
 
 /**
  * Created by Jacob on 05-06-2017.
  */
 public class CreateOrder extends JPanel {
-    private JXDatePicker date;
+    private JXDatePicker dateField;
     private JLabel dateLabel;
     private JLabel headingLabel;
     private JButton searchButton;
     private JComboBox<String> sizesBox;
     private JLabel sizesLabel;
+    private Messages messages;
 
     /**
      * Creates new form MainPanel
      */
     public CreateOrder() {
+        messages = new Messages();
         initComponents();
+        searchBoxes();
+    }
+
+    private void searchBoxes() {
+        searchButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (!(dateField.getDate() == null)) {
+                    Date date = new Date(dateField.getDate().getTime());
+                    int size = Integer.parseInt(sizesBox.getSelectedItem().toString());
+
+                    AvailableBoxes availableBoxes = new AvailableBoxes(date, size);
+
+                    availableBoxes.setVisible(true);
+
+                } else {
+                    messages.errorMessage("Du skal bruge kalender til at vælge en dato for reservation af boks!");
+                }
+
+            }
+        });
     }
 
     private void initComponents() {
 
         headingLabel = new javax.swing.JLabel();
-        date = new org.jdesktop.swingx.JXDatePicker();
+        dateField = new org.jdesktop.swingx.JXDatePicker();
         dateLabel = new javax.swing.JLabel();
         sizesBox = new javax.swing.JComboBox<>();
         sizesLabel = new javax.swing.JLabel();
@@ -55,7 +83,7 @@ public class CreateOrder extends JPanel {
                                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
                                                 .addComponent(searchButton, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                                                 .addComponent(sizesBox, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.PREFERRED_SIZE, 206, javax.swing.GroupLayout.PREFERRED_SIZE))
-                                        .addComponent(date, javax.swing.GroupLayout.PREFERRED_SIZE, 206, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addComponent(dateField, javax.swing.GroupLayout.PREFERRED_SIZE, 206, javax.swing.GroupLayout.PREFERRED_SIZE)
                                         .addGroup(layout.createSequentialGroup()
                                                 .addGap(9, 9, 9)
                                                 .addComponent(headingLabel)))
@@ -69,7 +97,7 @@ public class CreateOrder extends JPanel {
                                 .addGap(69, 69, 69)
                                 .addComponent(dateLabel)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(date, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addComponent(dateField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                                 .addComponent(sizesLabel)
                                 .addGap(3, 3, 3)
